867f1a8dc4e7a7389f5e60b95dc085c40cffc5d9
AndreySyp <sammyva199@gmail.com>
2023-02-12 18:43:38 +0300

    Added method for differentiation

--- activfun.py
+++ activfun.py
@@ -9,6 +9,17 @@ def sigmoid_logistic(x: numpy.ndarray, alpha: float = 1.0) -> numpy.ndarray:
     Применяет функцию активации ко всем элементам массива
     :param x: Массив значений
     :param alpha: Параметр насыщения
-    :return: Массив измененных значений
+    :return: Массив значений в точках
     """
     return numpy.array([1 / (numpy.exp(-alpha * i) + 1) for i in x])
+
+
+def diff_sigmoid_logistic(x: numpy.ndarray, alpha: float = 1.0) -> numpy.ndarray:
+    """
+    Считает производную в точке для всех элементов
+    :param x: Массив значений
+    :param alpha: Параметр насыщения
+    :return: Массив производных в точках
+    """
+    f = sigmoid_logistic(x, alpha)
+    return alpha * f * (1 - f)

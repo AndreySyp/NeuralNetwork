ed327a17cacf7ecb9dc1e0d8b0724f390fc03b2e
AndreySyp <sammyva199@gmail.com>
2023-02-12 18:54:26 +0300

    Refactoring

--- Singlelayer_perceptrons.py
+++ Singlelayer_perceptrons.py
@@ -1,34 +1,7 @@
+from activfun import sigmoid_logistic
+from generic_methods import neuron_state, w_recalculation
 import data_worker
 import numpy
-from activfun import sigmoid_logistic
-
-
-def neuron_state(x: numpy.ndarray, w: numpy.ndarray) -> numpy.ndarray:
-    """
-    Состояние нейрона
-    :param x: Вектор сигналов
-    :param w: Весовые коэффициенты
-    :return: массив состояний
-    """
-    s = numpy.zeros(data_worker.AMOUNT_Y)
-    s += w[0]
-    s += numpy.dot(x, w[1:])
-
-    return s
-
-
-def w_recalculation(x: numpy.ndarray, w: numpy.ndarray, error: numpy.ndarray,
-                    v: float = 9):
-    """
-    Коррекция старых значений весовых коэффициентов каждого нейрона
-    :param x: Вектор сигналов
-    :param w: Весовые коэффициенты
-    :param error: Погрешности выходных значений
-    :param v: Коэффициент скорости обучения
-    """
-    w[0] += v * error
-    for i in range(1, len(w)):
-        w[i] += x[i - 1] * error * v
 
 
 def calculation_start(data: numpy.ndarray, epoch: int = 10, v: float = 0.9, y: int = 1, alpha: float = 1):
@@ -40,6 +13,7 @@ def calculation_start(data: numpy.ndarray, epoch: int = 10, v: float = 0.9, y: i
     :param y: Количество выходных значений (y)
     :param alpha: Параметр насыщения
     """
+
     data_worker.AMOUNT_Y = y  # Количество столбцов
     x, y = data_worker.array_splitting(data)  # Разделяем массив
     w = numpy.random.uniform(low=-0.2, high=0.2, size=(len(x[0]) + 1, len(y[0])))  # Массив весов
@@ -72,20 +46,17 @@ def calculation_start(data: numpy.ndarray, epoch: int = 10, v: float = 0.9, y: i
                 w_history[ind] = numpy.hstack([w_history[ind], i])
 
         # Перемешиваем значения
-        union = numpy.hstack([x, y])
-        numpy.random.shuffle(union)
-        x, y = data_worker.array_splitting(union)
+        x, y = data_worker.array_reshuffle(x, y)
 
         # Информация для вывода в консоль
         global_error = numpy.sqrt(global_error / (len(y) * len(y[0])))
         print(f"Global error = {global_error}")
         print(f"Weight:\n{numpy.array(w_history)}")
         print("\n\n")
-
     print(f"Y on the last epoch:\n {numpy.array(y_history)}")
 
 
 if __name__ == "__main__":
-    array = data_worker.read("data\\single perceptrons\\met_norm.csv")
+    array = data_worker.read("data\\singlelayer perceptrons\\met_norm.csv")
     array = data_worker.normalization(array)
     calculation_start(array, y=2, epoch=10)

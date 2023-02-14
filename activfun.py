d85bdfe2c96a0eb7157fb775f89b49e886b1feaa
AndreySyp <sammyva199@gmail.com>
2023-02-14 15:19:22 +0300

    Added Gaussian function

--- activfun.py
+++ activfun.py
@@ -23,3 +23,26 @@ def diff_sigmoid_logistic(x: numpy.ndarray, alpha: float = 1.0) -> numpy.ndarray
     """
     f = sigmoid_logistic(x, alpha)
     return alpha * f * (1 - f)
+
+
+def gaussian(x: numpy.ndarray, alpha: float = 1.0, t: float = 1) -> numpy.ndarray:
+    """
+    Применяет функцию активации ко всем элементам массива
+    :param x: Массив значений
+    :param alpha: Параметр насыщения
+    :param t: Вертикальная ось симметрии
+    :return: Массив значений в точка
+    """
+    return numpy.array([1 / (numpy.exp(-alpha * (i - t)**2) + 1) for i in x])
+
+
+def diff_gaussian(x: numpy.ndarray, alpha: float = 1.0, t: float = 1) -> numpy.ndarray:
+    """
+    Применяет функцию активации ко всем элементам массива
+    :param x: Массив значений
+    :param alpha: Параметр насыщения
+    :param t: Вертикальная ось симметрии
+    :return: Массив значений в точка
+    """
+    f = gaussian(x, alpha, t)
+    return -2 * alpha * (x - t) * f

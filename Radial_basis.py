687c6a69898b895a83d45499f7c672ab70a130b9
AndreySyp <sammyva199@gmail.com>
2023-02-26 21:52:22 +0300

    First iteration RBF

--- Radial_basis.py
+++ Radial_basis.py
@@ -12,21 +12,33 @@ def calculation_start(data: numpy.ndarray, epoch: int = 10, v: float = 0.9, y: i
     :param y: Количество выходных значений (y)
     :param alpha: Параметр насыщения
     """
-    data_worker.AMOUNT_Y = y  # Количество столбцов
-    x, y = data_worker.array_splitting(data)  # Разделяем массив
+    # x = [0, 0.2, 0.4, 0.6, 0.8, 1]
+    # y = [1.00, 0.83, 0.71, 0.63, 0.56, 0.50]
+    # d = [0, 0.5, 1]
 
-    dote = [-2, -1, 0, 1, 2]
-    h = numpy.zeros((len(x), len(dote)))
-    for i in range(len(x)):
-        for j in range(len(dote)):
-            h[i][j] = activfun.gaussian(x[i], 0.22, dote[j])
+    x = [-0.5,	-0.4,	-0.3,	-0.2,	-0.1,	0,	0.1,	0.2,	0.3,	0.4,	0.5]
+    y = [0.111,	0.130,	0.149,	0.167,	0.184,	0.200,	0.216,	0.231,	0.245,	0.259,	0.273]
+    d = [-0.5, -0.3, -0.1, 0.1, 0.3, 0.5]
 
+    r = 0.22
+    alpha = 1 / (2 * r ** 2)
+
+    h = []
+    for ind_1, num_1 in enumerate(x):
+        h.append([])
+        for ind_2, num_2 in enumerate(d):
+            h[ind_1].append(numpy.exp(-alpha * (num_1 - num_2) ** 2))
+    h = numpy.array(h)
+    print(numpy.around(h, 3))
     h_t = numpy.transpose(h)
-    w = numpy.dot(numpy.dot(numpy.linalg.inv(numpy.dot(h_t, h)), h_t), y)
-    print(w)
+    w = numpy.dot(numpy.dot(numpy.linalg.inv(numpy.dot(h_t, h)), h_t), numpy.transpose(y))
+    print(numpy.around(w, 3))
+
+    test = 0.35
+    print(sum(([numpy.exp(-alpha * (test - num) ** 2) for ind, num in enumerate(d)] * w)))
 
 
 if __name__ == "__main__":
     array = data_worker.read("data\\met_denorm_radial.csv")
-    # array = data_worker.normalization(array)
+    array = data_worker.normalization(array)
     calculation_start(array, y=1, epoch=10)
